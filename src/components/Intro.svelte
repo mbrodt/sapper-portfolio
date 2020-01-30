8abcc16049d76ec2c8e46df2991ba75e553ab24a
Mads <mkrn@itu.dk>
2020-01-30 23:05:32 +0100

    add intro section

---
+++ src/components/Intro.svelte
@@ -0,0 +1,36 @@
+<script>
+  // your script goes here
+  import SocialIcons from "./SocialIcons.svelte";
+</script>
+
+<style>
+
+</style>
+
+<div class="pb-12 md:max-w-3xl md:mx-auto lg:max-w-full">
+  <SocialIcons />
+  <p class="text-sm font-semibold text-gray-600 uppercase tracking-wider mt-2">
+    Front end
+  </p>
+  <h1
+    class="mt-2 text-4xl leading-tight xl:text-6xl font-semibold font-display
+    text-gray-800">
+    Hi, I'm Mads
+  </h1>
+  <p class="mt-3 text-lg max-w-xl lg:max-w-3xl text-gray-700 xl:text-2xl">
+    I'm a front-end developer from Copenhagen. I design, create and support
+    websites and applications for clients and for fun. I love using modern
+    technology to create exceptional user experiences, and always strive to
+    create high quality products.
+  </p>
+
+  <!-- <div class="mt-2">
+    <p
+      class="text-base xl:text-lg max-w-xl lg:max-w-3xl font-medium
+      text-gray-700">
+      Have a look at some of my recent projects - maybe we'll be a good fit to
+      work together. And don't hesitate to get in contact if you have any
+      questions!
+    </p>
+  </div> -->
+</div>
