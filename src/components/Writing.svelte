601ff053c20dca0a985cb80a97fd9571c28cb451
Mads Brodt Nielsen <madsn@ajukreizi.com>
2020-02-06 17:08:40 +0100

    add blog posts to frontpage

--- src/components/Writing.svelte
+++ src/components/Writing.svelte
@@ -1,9 +1,17 @@
 <script>
   import SectionHeading from "./ui/SectionHeading.svelte";
+  export let posts;
 </script>
 
-<style>
-  /* your styles go here */
-</style>
-
 <SectionHeading>Writing</SectionHeading>
+<ul>
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
+</ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,22 @@
+<script context="module">
+  export function preload() {
+    return this.fetch(`blog.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
 <script>
   import Projects from "../components/Projects.svelte";
   import Intro from "../components/Intro.svelte";
   import Skills from "../components/Skills.svelte";
   import Writing from "../components/Writing.svelte";
   import Contact from "../components/Contact.svelte";
+  import SectionHeading from "../components/ui/SectionHeading.svelte";
+
+  export let posts;
 </script>
 
 <svelte:head>
@@ -18,6 +31,6 @@
   <Intro />
   <Projects />
   <Skills />
-  <!-- <Writing /> -->
+  <Writing {posts} />
   <Contact />
 </div>
