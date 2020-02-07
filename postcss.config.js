9cf88e8e9655f5fbe93670657b3e1f80cb578050
Mads Brodt Nielsen <madsn@ajukreizi.com>
2020-02-07 12:34:39 +0100

    whitelist classes that are dynamically added (so they dont get purged)

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,16 @@
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require("tailwindcss");
 // TODO purgecss is not actually purging
 // only needed if you want to purge
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.svelte', './src/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelistPatterns: [/scale/, /opacity/]
+});
 
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.config.js'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
-}
+};
