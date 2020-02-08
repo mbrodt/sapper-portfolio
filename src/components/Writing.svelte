43640b7a3163b925b9d781f3e789dc605c94aacf
Mads <mkrn@itu.dk>
2020-02-08 12:17:22 +0100

    style blog links

--- src/components/Writing.svelte
+++ src/components/Writing.svelte
@@ -1,17 +1,24 @@
 <script>
   import SectionHeading from "./ui/SectionHeading.svelte";
+  import Section from "./ui/Section.svelte";
   export let posts;
 </script>
 
-<SectionHeading>Writing</SectionHeading>
-<ul>
-  {#each posts as post}
-    <!-- we're using the non-standard `rel=prefetch` attribute to
+<Section>
+  <SectionHeading>Writing</SectionHeading>
+  <ul>
+    {#each posts as post}
+      <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-    <li>
-      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
-    </li>
-  {/each}
-</ul>
+      <li class="text-lg text-red-400 py-6 border-b-2 border-gray-200">
+        <a rel="prefetch" href="blog/{post.slug}">
+          {post.title}
+          <span>January 12, 2020</span>
+        </a>
+
+      </li>
+    {/each}
+  </ul>
+</Section>
