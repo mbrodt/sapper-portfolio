88f6b7000bd119ff10d251ae3f5cec0e24cf802f
Mads <mkrn@itu.dk>
2020-02-22 23:18:15 +0100

    add dots on large screens

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,33 +1,29 @@
 <script>
   // your script goes here
   import SocialIcons from "./SocialIcons.svelte";
+  import Dots from "./Dots.svelte";
 </script>
 
 <style>
 
 </style>
 
-<div class="pb-12 lg:max-w-full">
-  <SocialIcons />
-  <h1
-    class="mt-2 text-4xl sm:text-5xl leading-tight xl:text-6xl font-semibold
-    text-gray-800">
-    Hi, I'm Mads
-  </h1>
-  <p class="mt-3 text-lg max-w-xl lg:max-w-3xl xl:text-2xl">
-    I'm a front-end developer from Copenhagen. I design, create and support
-    websites and applications for clients and for fun. I love using modern
-    technology to create exceptional user experiences, and always strive to
-    create high quality products.
-  </p>
-
-  <!-- <div class="mt-2">
-    <p
-      class="text-base xl:text-lg max-w-xl lg:max-w-3xl font-medium
-      text-gray-700">
-      Have a look at some of my recent projects - maybe we'll be a good fit to
-      work together. And don't hesitate to get in contact if you have any
-      questions!
+<div class="pb-12 lg:max-w-full flex justify-between items-center">
+  <div>
+    <SocialIcons />
+    <h1
+      class="mt-2 text-4xl sm:text-5xl leading-tight xl:text-6xl font-semibold
+      text-gray-800">
+      Hi, I'm Mads
+    </h1>
+    <p class="mt-3 text-lg max-w-xl lg:max-w-3xl xl:text-2xl">
+      I'm a front-end developer from Copenhagen. I design, create and support
+      websites and applications for clients and for fun. I love using modern
+      technology to create exceptional user experiences, and always strive to
+      create high quality products.
     </p>
-  </div> -->
+  </div>
+  <div class="hidden lg:block">
+    <Dots />
+  </div>
 </div>
