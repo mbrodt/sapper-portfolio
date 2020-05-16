fef5c1a3dffed33f53872a770cff78224f0a7e4e
mbrodt <madsbrodt3@gmail.com>
2020-05-16 18:37:33 +0200

    add about page

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,5 +1,4 @@
 <script>
-  // your script goes here
   import SocialIcons from "./SocialIcons.svelte";
   import Dots from "./Dots.svelte";
 </script>
@@ -21,6 +20,25 @@
       websites and applications for clients and for fun. I love using modern
       technology to create exceptional user experiences, and always strive to
       create high quality products.
+      <a
+        class="text-red-400 hover:text-red-700 transition-colors duration-200
+        ease-out group mt-3 flex items-center"
+        href="/about">
+        Read more about me
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="transform transition-transform duration-200 ease-out
+          group-hover:translate-x-4 group-hover:scale-125 ml-2 inline w-5 h-5
+          sm:w-5 sm:h-5">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+      </a>
     </p>
   </div>
   <div class="hidden lg:block">
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,30 @@
+<script>
+import Timeline from "../components/Timeline.svelte"
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<div>
+<a href="/">
+      <svg
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        class="w-6 h-6 text-gray-500">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+      </svg>
 
-<p>This is the 'about' page. There's not much here.</p>
+    </a>
+    <h1
+      class="text-red-400 mt-2 text-3xl sm:text-4xl leading-tight xl:text-5xl
+      font-semibold">
+      About me
+    </h1>
+		<Timeline />
+		</div>
