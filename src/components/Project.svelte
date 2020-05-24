bbce4db7da15b97b3092ef6858559e589c0b410a
mbrodt <madsbrodt3@gmail.com>
2020-05-24 22:47:42 +0200

    refactor Project to its own component

---
+++ src/components/Project.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let project
+</script>
+
+<!-- {#if isVisible} -->
+<!-- transition:fly="{{ y: 200, duration: 500, delay: index * 500 }}" -->
+ <li  class="w-full">
+	<a href="{project.link}" target="_blank" rel="noopener noreferrer">
+		<img class="project w-full" src={'projects/' + project.src} alt="Project
+		showcase" />
+	</a>
+	<h3 class="tracking-wider uppercase text-gray-900 mt-4">
+		{project.title}
+	</h3>
+
+	<div class="-mx-1">
+		{#each project.tags as tag}
+		<span
+			class="bg-red-200 text-red-700 text-xs font-bold inline-block leading-4 px-2 py-1 mx-1 mt-2 rounded"
+		>
+			{tag}
+		</span>
+		{/each}
+	</div>
+	<p class="mt-4">{project.description}</p>
+</li>
+<!-- {/if} -->
