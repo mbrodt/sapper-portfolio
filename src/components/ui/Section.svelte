df4261d55252adfd5c0df477b6fb95b144f20d35
mbrodt <madsbrodt3@gmail.com>
2020-06-03 00:16:36 +0200

    add scrollreveal

--- src/components/ui/Section.svelte
+++ src/components/ui/Section.svelte
@@ -1,7 +1,30 @@
 <script>
+	import {onMount} from 'svelte'
+
 	export let id
+	let section
+
+	onMount(() => {
+		ScrollReveal({
+			origin: 'bottom',
+			distance: '20px',
+			duration: 500,
+			delay: 400,
+			rotate: { x: 0, y: 0, z: 0 },
+			opacity: 0,
+			scale: 1,
+			easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+			mobile: true,
+			reset: false,
+			useDelay: 'always',
+			viewFactor: 0,
+			viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  	}).reveal(section)
+	})
+
+
 </script>
 
-<section {id} class="mb-12">
+<section bind:this={section} {id} class="mb-12">
   <slot />
 </section>
