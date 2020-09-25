1f2d63c4620dcbae0fc6c56aab8cc80350d178b2
mbrodt <madsbrodt3@gmail.com>
2020-09-25 19:17:26 +0200

    update wording on confirm newsletter page

--- src/routes/confirm-newsletter.svelte
+++ src/routes/confirm-newsletter.svelte
@@ -13,17 +13,19 @@
   In order for me to send you emails, you first need to
   <strong>confirm your subscription.</strong>
   Check your email, and look for the confirmation link - and make sure to check
-  the spam folder if you can't see it ✌
+  the spam folder if you can't see it. If the email landed in the "Promotions"
+  tab in Gmail, you can drag it to the "Primary" tab to receive future emails
+  there ✌
   <br />
   <br />
   Thank you!
   <br />
   <br />
-  <a class="text-blue-600 font-bold link-underline" href="https://gmail.com">
+  <a class="text-red-400 font-bold link-underline" href="https://gmail.com">
     Open Gmail
   </a>
-  <span class="text-blue-600 font-bold mx-2">|</span>
-  <a class="text-blue-600 font-bold link-underline" href="http://outlook.com/">
+  <span class="text-red-400 font-bold mx-2">|</span>
+  <a class="text-red-400 font-bold link-underline" href="http://outlook.com/">
     Open Outlook
   </a>
 </p>
