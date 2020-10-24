3f204a50e74b9d407dde4a0335b37ad058de2083
mbrodt <madsbrodt3@gmail.com>
2020-10-24 12:14:35 +0200

    add transitions to newsletter page

--- src/routes/newsletter.svelte
+++ src/routes/newsletter.svelte
@@ -1,59 +1,75 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { onMount } from "svelte";
+  let visible = false;
+
+  onMount(() => {
+    visible = true;
+  });
+</script>
+
 <div class="max-w-3xl mx-auto">
-  <div class="text-center">
-    <h1
-      class="text-2xl sm:text-3xl md:text-4xl font-bold text-center
-        text-gray-900 leading-9 tracking-tight sm:leading-10">
-      Improve your coding career
-    </h1>
-    <p class="text-lg md:text-xl max-w-lg mx-auto">
-      No advertisements, no sponsors, no spam. Just high quality advice to aid
-      you on your coding journey.
-    </p>
-  </div>
-  <div
-    class="max-w-3xl mx-auto mt-8 lg:mt-12 px-4 pt-6 pb-2 sm:px-8 md:pt-12
-      md:pb-10 border border-gray-400">
-    <script
-      async
-      data-uid="f86832cf8a"
-      src="https://madsbrodt.ck.page/f86832cf8a/index.js">
-    </script>
-  </div>
-  <div class="mt-16 lg:mt-32 xl:mt-48 prose sm:prose-lg lg:prose-xl">
-    <h2 class="">
-      Career advice and front-end development content. Directly to your inbox.
-    </h2>
-    <div>
-      <p>
-        The newsletter is where I share my learnings, experiences and advice
-        from my own coding journey. Each issue is designed to provide as much
-        value as possible. No spam, and nothing annoying or unexpected. Just
-        valuable lessons to apply for yourself.
+  {#if visible}
+    <div class="text-center" in:fly={{ y: 70, duration: 600 }}>
+      <h1
+        class="text-2xl sm:text-3xl md:text-4xl font-bold text-center
+          text-gray-900 leading-9 tracking-tight sm:leading-10">
+        Improve your coding career
+      </h1>
+      <p class="text-lg md:text-xl max-w-lg mx-auto">
+        No advertisements, no sponsors, no spam. Just high quality advice to aid
+        you on your coding journey.
       </p>
-      <p>As a subscriber, you'll receive:</p>
-      <ul>
-        <li>
-          Long-form articles to improve your coding career – Once or twice per
-          month
-        </li>
-        <li>
-          Discounts and offers on my book or other courses down the road – A few
-          times per year
-        </li>
-        <li>
-          Updates about me, when I get a new job and the lessons from that –
-          Occasionally
-        </li>
-      </ul>
-      <p>
-        I'm sure you're already subscribed to plenty of newsletters, and you're
-        worried that this will be another one you sign up for and never read.
-        That won't be the case. My goal is to make you look forward to each and
-        every issue, because it'll contain so much value and actionable advice
-        that you'd be foolish not to read it. That's a high bar, but it's what
-        I'm striving to hit whenever I send an email to you.
-      </p>
-      <p>Sign up with the form above, and I'll be in touch shortly ✌</p>
     </div>
-  </div>
+    <div
+      class="max-w-3xl mx-auto mt-8 lg:mt-12 px-4 pt-6 pb-2 sm:px-8 md:pt-12
+        md:pb-10 border border-gray-400"
+      in:fly={{ y: 70, duration: 600, delay: 400 }}>
+      <script
+        async
+        data-uid="f86832cf8a"
+        src="https://madsbrodt.ck.page/f86832cf8a/index.js">
+      </script>
+    </div>
+    <div
+      class="mt-12 lg:mt-24 prose sm:prose-lg lg:prose-xl"
+      in:fly={{ y: 70, duration: 600, delay: 800 }}>
+      <h2 class="">
+        Career advice and front-end development content. Directly to your inbox.
+      </h2>
+      <div>
+        <p>
+          The newsletter is where I share my learnings, experiences and advice
+          from my own coding journey. Each issue is designed to provide as much
+          value as possible. No spam, and nothing annoying or unexpected. Just
+          valuable lessons to apply for yourself.
+        </p>
+        <p>As a subscriber, you'll receive:</p>
+        <ul>
+          <li>
+            Long-form articles to improve your coding career – Once or twice per
+            month
+          </li>
+          <li>
+            Discounts and offers on my book or other courses down the road – A
+            few times per year
+          </li>
+          <li>
+            Updates about me, when I get a new job and the lessons from that –
+            Occasionally
+          </li>
+        </ul>
+        <p>
+          I'm sure you're already subscribed to plenty of newsletters, and
+          you're worried that this will be another one you sign up for and never
+          read. That won't be the case. My goal is to make you look forward to
+          each and every issue, because it'll contain so much value and
+          actionable advice that you'd be foolish not to read it. That's a high
+          bar, but it's what I'm striving to hit whenever I send an email to
+          you.
+        </p>
+        <p>Sign up with the form above, and I'll be in touch shortly ✌</p>
+      </div>
+    </div>
+  {/if}
 </div>
