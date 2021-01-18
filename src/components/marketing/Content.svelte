e009b29c0dfb5445137e0030ecb927e71b989654
mbrodt <madsbrodt3@gmail.com>
2021-01-18 19:18:54 +0100

    Update sales copy

--- src/components/marketing/Content.svelte
+++ src/components/marketing/Content.svelte
@@ -11,37 +11,37 @@
   <div class="prose prose-lg xl:prose-xl mx-auto max-w-3xl">
 
     <p>
-      I get this question a lot. Coding is a difficult field, and it can be very
-      overwhelming when you're early in your career. There's so much stuff to
-      learn, so many possible areas to focus on, and how on Earth are you
+      I get this question a lot. Coding is a difficult field, and it can feel
+      overwhelming - especially early in your career. There's so much to
+      learn, so many areas to focus on, and how are you
       supposed to land a job without any experience?
     </p>
 
     <p>
-      I've been a developer for several years now, and I still get overwhelmed
-      sometimes. But I've also learned a lot of valuable lessons that has helped
+      I've been a developer for several years now, and I still get overwhelmed. But I've also learned valuable lessons that has helped
       me make tremendous progress in a short amount of time.
     </p>
 
     <p>
-      A lot of the difficulties we encounter when learning to code is because
-      our mindset isn't right. Coding is unique, and so it takes time for our brains to adjust to the field.
+      Most of the difficulties we encounter when learning to code is because
+      our mindset isn't right. Coding is unique, so it takes time for our brains to adjust.
     </p>
 
     <p>
-      But once we do, we're able to improve our skills, be more productive, deal
+      But once it does, we're able to improve our skills, be more productive, deal
       with self-doubt, land a job, and actually solve problems for real people
       and businesses. All while earning good money, too!
     </p>
 
     <p>
-      That is exactly why I wrote this book. I want to help you reframe your
+      That is why I wrote this book. I want to help you reframe your
       mindset, through tactical advice and effective practices. This book shares
       everything I know about being a professional developer, improving your
-      skills, and landing your dream development job. Jam-packed with actionable
+      skills, and landing your dream development job.
+		</p>
+		<p>Jam-packed with actionable
       advice and real-world experiences, I want to help you be the best
-      developer you can be.
-    </p>
+      developer you can be.</p>
 
     <p>Let's get started!</p>
 
