6824e395dece6640923581818b88285f608fcb7f
mbrodt <madsbrodt3@gmail.com>
2022-02-10 00:39:14 +0100

    Update book links

--- src/components/marketing/Pricing.svelte
+++ src/components/marketing/Pricing.svelte
@@ -1,62 +1,56 @@
 <div id="pricing" class="bg-indigo-900">
   <div
-    class="max-w-5xl mx-auto px-4 lg:px-8 text-xl py-16 lg:py-24 my-8 lg:my-24
-      xl:my-32"
+    class="max-w-5xl px-4 py-16 mx-auto my-8 text-xl lg:px-8 lg:py-24 lg:my-24 xl:my-32"
   >
     <div class="text-center">
-      <h2 class="text-4xl md:text-5xl text-white font-bold mt-0 mb-0">
+      <h2 class="mt-0 mb-0 text-4xl font-bold text-white md:text-5xl">
         Are you ready to Master the Coding Mindset 🔥?
       </h2>
-      <p class="text-2xl md:text-3xl text-blue-200 font-semibold">
+      <p class="text-2xl font-semibold text-blue-200 md:text-3xl">
         Choose the package that works for you.
       </p>
     </div>
 
     <div
-      class="mt-12 block lg:flex lg:items-center lg:flex-row-reverse lg:mt-16"
+      class="block mt-12 lg:flex lg:items-center lg:flex-row-reverse lg:mt-16"
     >
       <div
-        class="bg-white shadow-2xl rounded-lg max-w-xl mx-auto lg:min-w-0
-          lg:w-3/5 relative overflow-hidden"
+        class="relative max-w-xl mx-auto overflow-hidden bg-white rounded-lg shadow-2xl lg:min-w-0 lg:w-3/5"
       >
         <div class="px-8 py-6 bg-gray-200">
           <h3
-            class="text-indigo-900 uppercase tracking-wide text-lg sm:text-xl
-              text-center font-bold my-0"
+            class="my-0 text-lg font-bold tracking-wide text-center text-indigo-900 uppercase sm:text-xl"
           >
             The Complete Package
           </h3>
         </div>
 
-        <div class="px-4 sm:px-8 pb-8 text-base md:text-lg">
-          <div class="text-center py-6 lg:py-4">
+        <div class="px-4 pb-8 text-base sm:px-8 md:text-lg">
+          <div class="py-6 text-center lg:py-4">
             <span
-              class="line-through opacity-75 inline-flex items-center text-xl
-                md:text-2xl font-semibold text-grey-dark mr-3 sm:mr-4"
+              class="inline-flex items-center mr-3 text-xl font-semibold line-through opacity-75 md:text-2xl text-grey-dark sm:mr-4"
             >
               <span class="text-xl">$</span>
               <span>49</span>
             </span>
             <span
-              class="inline-flex items-center text-5xl md:text-6xl font-bold
-                text-gray-900 mr-3 sm:mr-4"
+              class="inline-flex items-center mr-3 text-5xl font-bold text-gray-900 md:text-6xl sm:mr-4"
             >
               <span class="text-2xl md:text-3xl">$</span>
               <span>0</span>
             </span>
             <span
-              class="inline-flex items-center text-xl md:text-2xl font-bold
-                text-gray-900 mr-3 sm:mr-4"
+              class="inline-flex items-center mr-3 text-xl font-bold text-gray-900 md:text-2xl sm:mr-4"
             >
               USD
             </span>
           </div>
           <div>
-            <ul class="list-reset px-2">
+            <ul class="px-2 list-reset">
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -73,13 +67,14 @@
                   </svg>
                 </span>
                 <span>
-                  <strong>The 118-page book</strong> in PDF and EPUB format
+                  <strong>The 118-page book</strong>
+                  in PDF and EPUB format
                 </span>
               </li>
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -96,15 +91,17 @@
                   </svg>
                 </span>
                 <span>
-                  Jam-packed with <strong>practical examples</strong> and <strong
-                  >actionable advice</strong> that will help you throughout your
-                  entire development career
+                  Jam-packed with
+                  <strong>practical examples</strong>
+                  and
+                  <strong>actionable advice</strong>
+                  that will help you throughout your entire development career
                 </span>
               </li>
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -121,13 +118,15 @@
                   </svg>
                 </span>
                 <span>
-                  Over <strong>70+ minutes</strong> of video content
+                  Over
+                  <strong>70+ minutes</strong>
+                  of video content
                 </span>
               </li>
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -144,15 +143,16 @@
                   </svg>
                 </span>
                 <span>
-                  Two videos on <strong>how to land a job,</strong> based on my own
-                  story from three different development jobs
+                  Two videos on
+                  <strong>how to land a job,</strong>
+                  based on my own story from three different development jobs
                 </span>
               </li>
 
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -169,14 +169,15 @@
                   </svg>
                 </span>
                 <span>
-                  Two videos on <strong>problem solving,</strong> and how to improve
-                  this key skill for developers
+                  Two videos on
+                  <strong>problem solving,</strong>
+                  and how to improve this key skill for developers
                 </span>
               </li>
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -193,20 +194,18 @@
                   </svg>
                 </span>
                 <span>
-                  A personal <strong>resume review.</strong> Send me your resume/CV
-                  and I'll offer suggestions for improvement
+                  A personal
+                  <strong>resume review.</strong>
+                  Send me your resume/CV and I'll offer suggestions for
+                  improvement
                 </span>
               </li>
             </ul>
 
-            <div class="text-center mt-12">
+            <div class="mt-12 text-center">
               <a
-                class="block w-full text-xl sm:text-2xl bg-indigo-500 rounded-lg
-                  focus:outline-none hover:bg-indigo-700 focus:bg-indigo-700
-                  text-white font-semibold px-6 py-4 sm:py-5 transition-colors
-                  duration-100 ease-in-out"
-                href="https://gum.co/mastering-coding-mindset-complete/launch"
-                data-gumroad-single-product="true"
+                class="block w-full px-6 py-4 text-xl font-semibold text-white transition-colors duration-100 ease-in-out bg-indigo-500 rounded-lg sm:text-2xl focus:outline-none hover:bg-indigo-700 focus:bg-indigo-700 sm:py-5"
+                href="https://mads.fyi/book"
                 target="_blank"
               >
                 Buy Now
@@ -217,47 +216,42 @@
       </div>
 
       <div
-        class="bg-white shadow rounded-lg mt-16 max-w-xs mx-auto lg:max-w-full
-          lg:rounded-r-none lg:min-w-0 lg:w-2/5 lg:mt-0 overflow-hidden"
+        class="max-w-xs mx-auto mt-16 overflow-hidden bg-white rounded-lg shadow lg:max-w-full lg:rounded-r-none lg:min-w-0 lg:w-2/5 lg:mt-0"
       >
         <div class="px-8 py-6 bg-gray-200">
           <h3
-            class="text-indigo-900 uppercase tracking-wide text-xl text-center
-              font-bold my-0"
+            class="my-0 text-xl font-bold tracking-wide text-center text-indigo-900 uppercase"
           >
             The Book
           </h3>
         </div>
 
-        <div class="px-4 sm:px-8 pb-8 text-base md:text-lg">
-          <div class="text-center py-4">
+        <div class="px-4 pb-8 text-base sm:px-8 md:text-lg">
+          <div class="py-4 text-center">
             <span
-              class="line-through opacity-75 inline-flex items-center text-lg
-                md:text-xl font-semibold text-grey-dark mr-2 sm:mr-3"
+              class="inline-flex items-center mr-2 text-lg font-semibold line-through opacity-75 md:text-xl text-grey-dark sm:mr-3"
             >
               <span class="text-xl">$</span>
               <span>25</span>
             </span>
             <span
-              class="inline-flex items-center text-4xl md:text-5xl font-bold
-                text-gray-900 mr-2 sm:mr-3"
+              class="inline-flex items-center mr-2 text-4xl font-bold text-gray-900 md:text-5xl sm:mr-3"
             >
               <span class="text-xl md:text-2xl">$</span>
               <span>0</span>
             </span>
             <span
-              class="inline-flex items-center text-lg md:text-xl font-bold
-                text-gray-900 mr-2 sm:mr-3"
+              class="inline-flex items-center mr-2 text-lg font-bold text-gray-900 md:text-xl sm:mr-3"
             >
               USD
             </span>
           </div>
           <div>
-            <ul class="list-reset px-2 sm:px-6">
+            <ul class="px-2 list-reset sm:px-6">
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -274,13 +268,14 @@
                   </svg>
                 </span>
                 <span>
-                  <strong>The 118-page book</strong> in PDF and EPUB format
+                  <strong>The 118-page book</strong>
+                  in PDF and EPUB format
                 </span>
               </li>
               <li class="flex items-start mb-4">
-                <span class="mr-4 mt-1">
+                <span class="mt-1 mr-4">
                   <svg
-                    class="h-5 w-5 block"
+                    class="block w-5 h-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -297,21 +292,19 @@
                   </svg>
                 </span>
                 <span>
-                  Jam-packed with <strong>practical examples</strong> and <strong
-                  >actionable advice</strong> that will help you throughout your
-                  entire development career
+                  Jam-packed with
+                  <strong>practical examples</strong>
+                  and
+                  <strong>actionable advice</strong>
+                  that will help you throughout your entire development career
                 </span>
               </li>
             </ul>
 
-            <div class="text-center mt-12">
+            <div class="mt-12 text-center">
               <a
-                class="w-full text-lg sm:text-xl block rounded-lg
-                  focus:outline-none bg-gray-300 text-indigo-500
-                  hover:bg-gray-400 focus:bg-gray-400 transition-colors
-                  duration-100 ease-in-out font-semibold px-6 py-3 sm:py-4"
-                href="https://gum.co/mastering-coding-mindset/launch"
-                data-gumroad-single-product="true"
+                class="block w-full px-6 py-3 text-lg font-semibold text-indigo-500 transition-colors duration-100 ease-in-out bg-gray-300 rounded-lg sm:text-xl focus:outline-none hover:bg-gray-400 focus:bg-gray-400 sm:py-4"
+                href="https://mads.fyi/book"
                 target="_blank"
               >
                 Buy Now
